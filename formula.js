1f183520fdbd6621016acc62a08d429188b32fdf
Serafim Gerasimov <serafimgerasimov44@gmail.com>
2024-06-22 02:17:23 +0500

    feat: monorepo init

---
+++ formula.js
@@ -0,0 +1,57 @@
+const paramsSchema = {
+  properties: {
+    name: {
+      type: "string",
+    },
+    symbol: {
+      type: "string",
+    },
+    image: {
+      type: "string",
+      format: "uri",
+    },
+    description: {
+      type: "string",
+    },
+    network: {
+      type: "string",
+      enum: ["testnet", "mainnet"],
+    },
+  },
+  required: ["name", "symbol", "network"],
+};
+
+function deploy(params) {
+  const [address] = api.blueprint.script({
+    envs: {
+      TOKEN_NAME: params.name,
+      TOKEN_SYMBOL: params.symbol,
+      TOKEN_DESCRIPTION: params.description,
+      TOKEN_IMAGE:
+        params.image ||
+        "https://assets.transak.com/images/cryptoCurrency/notcoin_large.jpg",
+    },
+    outputs: [
+      {
+        name: "address",
+        extract: {
+          type: "regex",
+          expr: "Contract deployed at address (?<address>[A-Za-z0-9_-]+)",
+          groupName: "address",
+        },
+      },
+    ],
+  });
+
+  api.offchain.deploy({
+    details: {
+      envs: {
+        TOKEN_ADDRESS: address,
+        NETWORK: params.network,
+      },
+      flags: {
+        build: true,
+      },
+    },
+  });
+}
